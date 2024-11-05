0f14c9a9328b61591cfe514f671eaa50c427ab0e
KroSheChKa <104899233+KroSheChKa@users.noreply.github.com>
2024-11-06 00:44:09 +0300

    Optimization

--- pointers1/pointers1.js
+++ pointers1/pointers1.js
@@ -1,19 +1,22 @@
-let offset = 30;
+let offset = 35;
 let pointers = [];
 let x;
 let y;
 let radius = 200;
+let offsetY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight*0.997);
+  frameRate(60);
   x = width / offset;
   y = height / offset;
+  offsetY = Math.floor(y);
   for (let i = 0; i < x; i += 1) {
     for (let j = 0; j < y-1; j += 1) {
-      let position = createVector(i * offset, j * offset);
-      let angle = random(TWO_PI); // Случайный угол от 0 до 2π
-      let direction = p5.Vector.fromAngle(angle).mult(8); // Создаем вектор длиной 8
-      pointers[i * Math.floor(y) + j] = new Pointer(position, direction);
+      let position = createVector(offset*(i + 0.5), offset*(j + 0.5));
+      let angle = random(TWO_PI);
+      let direction = p5.Vector.fromAngle(angle).mult(8);
+      pointers[i * offsetY + j] = new Pointer(position, direction);
     }
   }
 }
@@ -22,8 +25,8 @@ function draw() {
   background(16);
   for (let i = 0; i < x; i += 1) {
     for (let j = 0; j < y-1; j += 1) {
-      pointers[i * Math.floor(y) + j].move();
-      pointers[i * Math.floor(y) + j].show();
+      pointers[i * offsetY + j].move();
+      pointers[i * offsetY + j].show();
     }
   }
 }
@@ -31,28 +34,25 @@ function draw() {
 class Pointer {
   constructor(position, direction) {
     this.position = position;
-    this.direction = direction; // Задаем случайное начальное направление
+    this.direction = direction;
   }
 
   move() {
     let mousePosition = createVector(mouseX, mouseY);
-    let distance = dist(mousePosition.x, mousePosition.y, this.position.x, this.position.y);
+    let dx = mousePosition.x - this.position.x;
+    let dy = mousePosition.y - this.position.y;
+    let distanceSq = dx * dx + dy * dy;
     
-    if (distance < radius) {
-      // Если в пределах радиуса, обновляем направление
+    if (distanceSq < radius * radius) {
       this.direction = p5.Vector.sub(mousePosition, this.position);
       this.direction.normalize();
       this.direction.mult(8);
     }
-    // Если вне радиуса, не обновляем `direction`, линия остается в последнем направлении
   }
 
   show() {
-    push();
-    translate(this.position.x, this.position.y);
     stroke(255);
     strokeWeight(2);
-    line(0, 0, this.direction.x, this.direction.y);
-    pop();
+    line(this.position.x, this.position.y, this.position.x + this.direction.x, this.position.y + this.direction.y);
   }
 }
