bcb84e8dc5b6e18138803740689fd8f1b10b3cf2
KroSheChKa <104899233+KroSheChKa@users.noreply.github.com>
2024-11-15 02:56:03 +0300

    Wallpaper engine support
    + optimization

--- pointers1/pointers1.js
+++ pointers1/pointers1.js
@@ -1,30 +1,90 @@
-let offset = 40;
 let pointers = [];
 let x;
 let y;
-let radius = 300;
 let prev_mouseX = -1, prev_mouseY = -1;
 let floored_mouseX, floored_mouseY;
-let freeze; // canvas to store frozen lines
+let freeze;
 let frozen = false;
-let mag_divisor = 141 // affects on how fast the line lengthen 
+let lastOffset;
+
+// walpaper engine
+let backgroundColor = [16, 16, 16];
+let lineColor = [255, 255, 255];
+let lineWidth = 2
+let radius = 300;
+let offset = 40;
+let lineLength = 9;
+let magDivisor = 140;
+
+window.wallpaperPropertyListener = {
+  applyUserProperties: function(properties) {
+    if (properties.background_color) {
+      let color = properties.background_color.value.split(' ');
+      backgroundColor = color.map(c => Math.floor(c * 255));
+    }
+    if (properties.line_color1) {
+      let color = properties.line_color1.value.split(' ');
+      lineColor = color.map(c => Math.floor(c * 255));
+    }
+    if (properties.line_width) {
+      lineWidth = properties.line_width.value;
+    }
+    if (properties.lines_offset) {
+      offset = properties.lines_offset.value;
+      if (offset !== lastOffset) {
+        initializePointers();
+        lastOffset = offset;
+      }
+    }
+    if (properties.range) {
+      radius = properties.range.value;
+    }
+    if (properties.line_length) {
+      lineLength = properties.line_length.value;
+    }
+    if (properties.mag_divisor) {
+      magDivisor = properties.mag_divisor.value;
+    }
+  }
+};
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight * 0.996);
   freeze = createGraphics(windowWidth, windowHeight * 0.996);
   frameRate(60);
+  initializePointers();
+}
+
+function initializePointers() {
+  background(backgroundColor[0], backgroundColor[1], backgroundColor[2])
+  freeze.background(backgroundColor[0], backgroundColor[1], backgroundColor[2])
+  pointers = [];
   x = width / offset;
   y = height / offset;
   let offsetY = Math.floor(y);
+  
   for (let i = 0; i < x; i += 1) {
     for (let j = 0; j < offsetY; j += 1) {
-      let position = createVector(offset*(i + 0.5), offset*(j + 0.5));
+      let position = createVector(offset * (i + 0.5), offset * (j + 0.5));
       let angle = random(TWO_PI);
-      let direction = p5.Vector.fromAngle(angle).mult(8);
-      pointers[i * offsetY + j] = new Pointer(position, direction);
+      let direction = p5.Vector.fromAngle(angle).mult(lineLength);
+      let index = i * offsetY + j
+      pointers[index] = new Pointer(position, direction);
+      pointers[index].move();
+      pointers[index].show();
     }
   }
+  freeze.clear();
+  freeze.image(get(), 0, 0);
+  frozen = true;
 }
+
+
+function mousePressed() {
+  initializePointers();
+}
+
 function draw() {
   floored_mouseX = floor(mouseX);
   floored_mouseY = floor(mouseY);
@@ -33,11 +93,12 @@ function draw() {
 
     frozen = false;
 
-    background(16);
+    background(backgroundColor[0], backgroundColor[1], backgroundColor[2]);
     for (let i = 0; i < pointers.length; i++) {
-      pointers[i].move();
+      pointers[i].move();      
       pointers[i].show();
     }
+    
     if (!frozen) {
       freeze.clear();
       freeze.image(get(), 0, 0);
@@ -66,15 +127,15 @@ class Pointer {
     let out_of_radius = distanceSq < radius * radius
     
     if (out_of_radius) {
-      let mag = sqrt(distanceSq) / mag_divisor;
-      this.direction.set(dx, dy).normalize().mult(9 + mag);
+      let mag = sqrt(distanceSq) / magDivisor;
+      this.direction.set(dx, dy).normalize().mult(lineLength + mag);
     }
     return out_of_radius
   }
 
   show() {
-    stroke(255);
-    strokeWeight(2);
+    stroke(lineColor[0], lineColor[1], lineColor[2]);
+    strokeWeight(lineWidth);
     line(this.position.x, this.position.y, this.position.x + this.direction.x, this.position.y + this.direction.y);
   }
 }
